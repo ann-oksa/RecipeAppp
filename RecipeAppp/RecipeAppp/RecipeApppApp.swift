ce51de17b112306e7d389bbbc18e62797442cc41
Hanna <oksanichenko@magora.co.uk>
2025-01-31 11:56:28 -0800

    init commit

---
+++ RecipeAppp/RecipeAppp/RecipeApppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApppApp.swift
+//  RecipeAppp
+//
+//  Created by Hanna on 1/31/2025.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
